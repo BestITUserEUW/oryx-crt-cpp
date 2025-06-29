a4398f029d9ffdd644c7b6a5f67e69603bfa78dd
Philemon Benner <philemon.benner@arrowtec.de>
2025-06-29 03:49:42 +0200

    added function tracer

---
+++ include/oryx/function_tracer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#if __has_include(<source_location>) && __has_include(<print>)
+
+    #include <print>
+    #include <source_location>
+    #include <string_view>
+    #include <functional>
+
+namespace oryx {
+
+class FunctionTracer {
+public:
+    constexpr FunctionTracer(const std::source_location loc = std::source_location::current())
+        : name_(loc.function_name()) {
+        std::println("Entering {}", name_);
+    }
+    ~FunctionTracer() { std::println("Exiting {}", name_); }
+
+private:
+    std::string_view name_;
+};
+
+}  // namespace oryx
+
+    #define ORYX_TRACE_FUNCTION() oryx::FunctionTracer __func_tracer__
+
+#endif
+
+#define ORYX_TRACE_FUNCTION() (void)0
