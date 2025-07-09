479ccc9209461c6ead86a8691bc52e1fd7ce923f
BestITUserEUW <phile.benner@gmail.com>
2025-07-09 03:14:50 +0200

    added simple from chars test

---
+++ tests/from_chars_test.cpp
@@ -0,0 +1,23 @@
+#include <oryx/from_chars.hpp>
+
+#include "doctest.hpp"
+
+using namespace oryx;
+
+TEST_CASE("Parsing valid values") {
+    CHECK(FromChars<int>("1").value() == 1);
+    CHECK(FromChars<int>("-1").value() == -1);
+    CHECK(FromChars<float>("1.2").value() == 1.2f);
+    CHECK(FromChars<float>("-1.2").value() == -1.2f);
+    CHECK(FromChars<double>("1.2").value() == 1.2);
+    CHECK(FromChars<double>("-1.2").value() == -1.2);
+    CHECK(FromChars<bool>("0").value() == false);
+    CHECK(FromChars<bool>("1").value() == true);
+}
+
+TEST_CASE("Parsing invalid values") {
+    CHECK_FALSE(FromChars<int>("why"));
+    CHECK_FALSE(FromChars<float>("are"));
+    CHECK_FALSE(FromChars<double>("you"));
+    CHECK_FALSE(FromChars<bool>("reading"));
+}
